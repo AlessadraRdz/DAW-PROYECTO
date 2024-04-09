192712e0783c42897fcc145efee4f99c30f28e78
AlessandraRdz <a20490230@itmexicali.edu.mx>
2024-04-08 21:04:52 -0700

    main

---
+++ src/controllers/userController.js
@@ -0,0 +1,164 @@
+//LOGEOO
+
+function login(req, res) {
+    res.render('vistasCrud/login');
+}
+function confirma(req, res) {
+    const { fname, password, rol } = req.body;
+    req.getConnection((err, conn) => {
+      const sql = "CALL verificarUsuario(?, ?, ?)";
+      conn.query(sql, [fname, password, rol], (err, results) => {  
+        const user = results[0][0];
+        res.redirect('/listar_libros');
+    })
+});
+}
+  
+
+//CARRITO
+function carrito(req, res) {
+    const datos = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO carrito SET ?', [datos], (err, rows) => {
+            res.redirect('/carrito');
+        })
+    });
+}
+
+
+
+//CRUD DE USUARIOS
+function listar(req, res) {
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM users', (err, user) => {
+            if (err) {
+                res.json(err);
+            }
+            res.render('vistasCrud/listar', { user });
+        })
+    });
+}
+
+function agregar(req, res) {
+    res.render('vistasCrud/agregar');
+}
+
+function guardar(req, res) {
+    const datos = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO users SET ?', [datos], (err, rows) => {
+            res.redirect('/listar');
+        })
+    });
+}
+
+function eliminar(req, res) {
+    const id = req.body.id;
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM users WHERE id = ?', [id], (err, rows) => {
+            res.redirect('/listar');
+        })
+    })
+}
+
+function editar(req, res) {
+    const id = req.params.id;
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM users WHERE id = ?', [id], (err, user) => {
+            if (err) {
+                res.json(err);
+            }
+            res.render('vistasCrud/editar', { user });
+        })
+    });
+}
+
+function actualizar(req, res) {
+    const id = req.params.id;
+    const datos = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE users SET ? WHERE id = ?', [datos, id], (err, rows) => {
+            res.redirect('/listar');
+        })
+    })
+}
+
+//CRUD DE LIBROS**************************************************************************************
+function listar_libro(req, res) {
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM libros', (err, libro) => {
+            if (err) {
+                res.json(err);
+            }
+            res.render('vistasCrud/listar_libro', { libro });
+        })
+    });
+}
+
+function agregar_libro(req, res) {
+    res.render('vistasCrud/agregar_libro');
+}
+
+function guardar_libro(req, res) {
+    const datos = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO Libros SET ?', [datos], (err, rows) => {
+            res.redirect('/listar_libro');
+        })
+    });
+}
+
+function eliminar_libro(req, res) {
+    const id = req.body.id;
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM libros WHERE id = ?', [id], (err, rows) => {
+            res.redirect('/listar_libro');
+        })
+    })
+}
+
+function editar_libro(req, res) {
+    const id = req.params.id;
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM libros WHERE id = ?', [id], (err, libro) => {
+            if (err) {
+                res.json(err);
+            }
+            res.render('vistasCrud/editar_libro', { libro });
+        })
+    });
+}
+
+function actualizar_libro(req, res) {
+    const id = req.params.id;
+    const datos = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('UPDATE libros SET ? WHERE id = ?', [datos, id], (err, rows) => {
+            res.redirect('/listar_libro');
+        })
+    })
+}
+
+
+
+module.exports = {
+    //LOGEO
+    login : login,
+    confirma : confirma,
+    //CARRITO
+    carrito : carrito,
+    //USUARIOS
+    listar: listar,
+    agregar: agregar,
+    guardar: guardar,
+    eliminar: eliminar,
+    editar: editar,
+    actualizar: actualizar,
+    //LIBROS
+    listar_libro: listar_libro,
+    agregar_libro: agregar_libro,
+    guardar_libro:guardar_libro ,
+    eliminar_libro: eliminar_libro,
+    editar_libro: editar_libro,
+    actualizar_libro: actualizar_libro,
+}
